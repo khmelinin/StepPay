f19a7805fc6d2ce314b6aa57ea2285062721e1de
eddyslav <76120105+eddyslav@users.noreply.github.com>
2021-05-19 19:58:22 +0300

    Добавлено метод для отримання валют

--- STEP_PAY/MainWindow.xaml.cs
+++ STEP_PAY/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using STEP_PAY.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +26,22 @@ namespace STEP_PAY
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async Task<List<Currency>> GetCurrencies()
+        {
+            HttpClient client = new HttpClient();
+            List<string> allowedCurrencies = new List<string> { "USD", "EUR", "RUB" };
+            var res = await client.GetAsync(Globals.CurrenciesUrl);
+
+            if (!res.IsSuccessStatusCode) throw new Exception("Couldn\'t receive currencies.");
+
+            var content = res.Content;
+            var currencies = from currency in JsonSerializer.DeserializeAsync<List<Currency>>(await content.ReadAsStreamAsync()).Result
+                             where allowedCurrencies.Find((str) => str == currency.cc) != null
+                             select currency;
+
+            return currencies.ToList();
         }
     }
 }
