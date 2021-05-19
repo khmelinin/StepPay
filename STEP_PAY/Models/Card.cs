574e39185192c1bdfe4df6dcb80a31bbb38e6757
Mali_v7bi <Mali_v7bi@student.itstep.org>
2021-05-19 19:51:15 +0300

    19.05.2021Misha(2)

--- STEP_PAY/Models/Card.cs
+++ STEP_PAY/Models/Card.cs
@@ -9,15 +9,20 @@ namespace STEP_PAY.Models
     class Card
     {
         public int Id { get; set; }
-
+        
         // Количество денег
         public int Money { get; set; }
+        // Текущяя валюта
+        public string Сurrency { get; set; }
 
         // Id пользователя
         public int UserId { get; set; }
 
         // Название Карты
         public string Title { get; set; }
+
+        // Избранное или нет
+        public bool Favorites { get; set; }
 
         // Номер Карты
         public string Number { get; set; }     
