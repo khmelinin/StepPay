473aba9377bd4ca19f33c7bf45a770177d2971d2
eddyslav <76120105+eddyslav@users.noreply.github.com>
2021-05-19 19:57:41 +0300

    Добавлено клас валют для десеріалізація

---
+++ STEP_PAY/Models/Currency.cs
@@ -0,0 +1,11 @@
+﻿namespace STEP_PAY.Models
+{
+    internal class Currency
+    {
+        public int r030 { get; set; }
+        public string txt { get; set; }
+        public double rate { get; set; }
+        public string cc { get; set; }
+        public string exchangedate { get; set; }
+    }
+}
