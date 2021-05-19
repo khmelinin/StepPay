d69e3600b263767261ebbb1cf4d369fbcb42aa72
eddyslav <76120105+eddyslav@users.noreply.github.com>
2021-05-19 19:57:55 +0300

    Добавлено клас для глобальних констант

---
+++ STEP_PAY/Globals.cs
@@ -0,0 +1,7 @@
+﻿namespace STEP_PAY
+{
+    internal static class Globals
+    {
+        public static string CurrenciesUrl = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json";
+    }
+}
