19ef1f5f91171be88a101a4a6d3265b563bb10c5
Ngassaki <cardorelngassaki@gmail.com>
2021-05-24 23:21:21 +0300

    Email + About us

--- STEP_PAY/ViewModel/SupportViewModel.cs
+++ STEP_PAY/ViewModel/SupportViewModel.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -17,6 +19,7 @@ namespace STEP_PAY.ViewModel
         private string hidden = "Hidden";
         private string textBoxValidation = "Hidden";
         private string textBoxContent;
+        private string emailText;
 
         private ICommand _showSupportBoxCommand;
         private ICommand _sendFeedBackCommand;
@@ -56,17 +59,47 @@ namespace STEP_PAY.ViewModel
         public ICommand SendFeedbackCommand => _sendFeedBackCommand ?? (_sendFeedBackCommand = new DelegateCommand(
            () =>
            {
-               if(string.IsNullOrEmpty(textBoxContent) && string.IsNullOrEmpty(textBoxContent))
+               if(string.IsNullOrEmpty(textBoxContent) 
+               && string.IsNullOrEmpty(textBoxContent) 
+               && string.IsNullOrEmpty(emailText) 
+               && string.IsNullOrWhiteSpace(emailText))
                {
                    TextBoxValidation = "Visible";
                }
                else
                {
-                   MessageBox.Show($"{TextBoxContent}");
+                   SendMessage(EmailText, TextBoxContent);
                    TextBoxContent = string.Empty;
+                   MessageBox.Show("Спасибо, свяжемся с вами в ближайшее время !");
 
                }
            }));
+
+        void SendMessage(string email , string body)
+        {
+            var toAddress = new MailAddress("cardorelngassaki@gmail.com");
+            var fromAddress = new MailAddress(email);
+            var smtp = new SmtpClient()
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address , "123456"),
+                TargetName="STARTTLS/smtp.gmail.com",
+                Timeout = 20000
+            };
+
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = "Feedback",
+                Body = body
+            })
+            {
+                //smtp.Send(message);
+            }
+        }
 
 
 
@@ -74,5 +107,6 @@ namespace STEP_PAY.ViewModel
         public string Hidden { get => hidden; set => SetProperty(ref hidden, value); }
         public string TextBoxValidation { get => textBoxValidation; set => SetProperty(ref textBoxValidation, value); }
         public string TextBoxContent { get => textBoxContent; set => SetProperty(ref textBoxContent, value); }
+        public string EmailText { get => emailText; set => SetProperty(ref emailText, value); }
     }
 }
