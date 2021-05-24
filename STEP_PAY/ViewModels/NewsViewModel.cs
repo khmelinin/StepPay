bbf2d225c2dfbfe5ceae6f3c011ed9e133da2627
PavloPinchuk <71881450+PavloPinchuk@users.noreply.github.com>
2021-05-24 20:53:13 +0300

    Доробив NewsView i NewsViewModel, підв'язав актуальний курс валют.

--- STEP_PAY/ViewModels/NewsViewModel.cs
+++ STEP_PAY/ViewModels/NewsViewModel.cs
@@ -1,8 +1,11 @@
-﻿using Prism.Commands;
+﻿using Newtonsoft.Json;
+using Prism.Commands;
+using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,18 +14,52 @@ using System.Windows.Navigation;
 
 namespace STEP_PAY.ViewModels
 {
-    public class NewsViewModel : INewsViewModel
+    internal static class Globals
     {
-       // private void Application_NavigationFailed(object sender,
-       //System.Windows.Navigation.NavigationFailedEventArgs e)
-       // {
-       //     if (e.Exception is System.Net.WebException)
-       //     {
-       //         MessageBox.Show("Сайт " + e.Uri.ToString() + " не доступен :(");
-       //         // Нейтрализовать ошибку, чтобы приложение продолжило свою работу
-       //         e.Handled = true;
-       //     }
-       // }
+        public static string CurrenciesUrl = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json";
+    }
+    public class Currency
+    {
+        [JsonProperty(PropertyName = "r030")]
+        public int CurrencyCode { get; set; }
+        [JsonProperty(PropertyName = "txt")]
+        public string FullName { get; set; }
+        [JsonProperty(PropertyName = "rate")]
+        public double Rate { get; set; }
+        [JsonProperty(PropertyName = "cc")]
+        public string ShortName { get; set; }
+        [JsonProperty(PropertyName = "exchangedate")]
+        public string ExchangeDate { get; set; }
+    }
+    public class NewsViewModel : BindableBase, INewsViewModel
+    {
+        private List<Currency> currencies = new List<Currency>();
+        public List<Currency> Currencies
+        {
+            get => currencies;
+            set
+            {
+                SetProperty(ref currencies, value);
+            }
+        }
+        public NewsViewModel()
+        {
+            GetCurrencies();
+        }
+
+        private async Task GetCurrencies()
+        {
+            HttpClient client = new HttpClient();
+            List<string> allowedCurrencies = new List<string> { "USD", "EUR", "RUB", "CZK", "CHF", "GBP", "PLN" };
+            var res = await client.GetAsync(Globals.CurrenciesUrl);
+            var content = res.Content;
+            var allCurrencies = JsonConvert.DeserializeObject<List<Currency>>(await content.ReadAsStringAsync());
+            var currencies = from currency in allCurrencies
+                             where allowedCurrencies.Find((str) => str == currency.ShortName) != null
+                             select currency;
+
+            Currencies = new List<Currency>(currencies);
+        }
         private void hyperlink_Click1()
         {
             try { Process.Start("https://www.pravda.com.ua/news/"); }
