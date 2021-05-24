13f8e830e4b30ec4faa127e58c009a02f49faec0
eddyslav <76120105+eddyslav@users.noreply.github.com>
2021-05-24 21:36:37 +0300

    Create DialogFactory.cs

---
+++ STEP_PAY/Helpers/DialogFactory.cs
@@ -0,0 +1,19 @@
+﻿using STEP_PAY.ViewModels;
+using STEP_PAY.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STEP_PAY.Helpers
+{
+    public static class DialogFactory
+    {
+        public static void ShowLoginView(LoginViewModel vm)
+        {
+            LoginView view = new LoginView() { DataContext = vm };
+            view.Show();
+        }
+    }
+}
