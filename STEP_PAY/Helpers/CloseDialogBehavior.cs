454baf60780733b30e03a5308507657803c95758
eddyslav <76120105+eddyslav@users.noreply.github.com>
2021-05-24 21:36:30 +0300

    Create CloseDialogBehavior.cs

---
+++ STEP_PAY/Helpers/CloseDialogBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace STEP_PAY.Helpers
+{
+    internal class CloseDialogBehavior
+    {
+        public static readonly DependencyProperty CloseDialogProperty
+            = DependencyProperty.RegisterAttached("CloseDialog", typeof(bool), typeof(CloseDialogBehavior), new PropertyMetadata(false, OnCloseDialogPropertyChanged));
+
+        public static bool GetCloseDialog(Window dlg)
+        {
+            return (bool)dlg.GetValue(CloseDialogProperty);
+        }
+
+        public static void SetCloseDialog(Window dlg, bool value)
+        {
+            dlg.SetValue(CloseDialogProperty, value);
+        }
+
+        private static void OnCloseDialogPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            Window dlg = d as Window;
+            bool isClose = (bool)e.NewValue;
+            if (dlg == null || !isClose)
+            {
+                return;
+            }
+
+            dlg.Close();
+        }
+    }
+}
