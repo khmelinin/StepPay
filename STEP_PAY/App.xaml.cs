f1995327b4b6b11085e5309b7bd6a65b58b2ae43
eddyslav <76120105+eddyslav@users.noreply.github.com>
2021-05-24 21:36:20 +0300

    changing startup logic

--- STEP_PAY/App.xaml.cs
+++ STEP_PAY/App.xaml.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using STEP_PAY.ViewModels;
+using STEP_PAY.Views;
 using System.Windows;
 
 namespace STEP_PAY
@@ -13,5 +9,11 @@ namespace STEP_PAY
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MainWindow = new MainView() { DataContext = new MainViewModel() };
+            MainWindow.Show();
+        }
     }
 }
