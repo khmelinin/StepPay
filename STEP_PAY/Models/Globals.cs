28e62d981b96c9dc702f73d7416d07a5cebabf42
Ngassaki <cardorelngassaki@gmail.com>
2021-05-29 00:17:21 +0300

    added currency

---
+++ STEP_PAY/Models/Globals.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Navigation;
+using Newtonsoft.Json;
+
+namespace STEP_PAY.Models
+{
+    internal static class Globals
+    {
+        public static string CurrenciesUrl = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json";
+    }
+
+    public class Currency
+    {
+        [JsonProperty(PropertyName = "r030")]
+        public int CurrencyCode { get; set; }
+        [JsonProperty(PropertyName = "txt")]
+        public string FullName { get; set; }
+        [JsonProperty(PropertyName = "rate")]
+        public double Rate { get; set; }
+        [JsonProperty(PropertyName = "cc")]
+        public string ShortName { get; set; }
+        [JsonProperty(PropertyName = "exchangedate")]
+        public string ExchangeDate { get; set; }
+    }
+}
